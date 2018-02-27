6c612d43132b8df9e07559fe9aad64808eb5767d
reactojango <adamgrizzard2@gmail.com>
2018-02-28 06:55:32 +0800

    feat(dwolla): added dwolla api client module

---
+++ functions/dwolla/api.js
@@ -0,0 +1,13 @@
+const dwolla = require('dwolla-v2');
+const config = require('../config');
+const client = new dwolla.Client({
+    key: config.dwolla.key,
+    secret: config.dwolla.secret,
+    environment: config.dwolla.environment
+});
+
+export function getAPIClient() {
+    return client.auth.client();
+}
+
+export default getAPIClient;
