20e79273665430a04c18fae97f1223b926703b68
reactojango <adamgrizzard2@gmail.com>
2018-02-28 01:05:03 +0800

    chore(app): config module added

---
+++ functions/config.js
@@ -0,0 +1,21 @@
+const functions = require('firebase-functions');
+const config = functions.config();
+
+const gmailConfig = config.gmail || {};
+const dwollaConfig = config.dwolla || {};
+const pushConfig = config.push || {};
+
+export default {
+    gmail: {
+        email: gmailConfig.email || '',
+        password: gmailConfig.password || ''
+    },
+    dwolla: {
+        key: dwollaConfig.key || '',
+        webkey: dwollaConfig.webkey || '',
+        secret: dwollaConfig.secret || ''
+    },
+    push: {
+        key: pushConfig.key || ''
+    }
+};
