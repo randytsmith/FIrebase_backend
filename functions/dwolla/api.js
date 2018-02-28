e1e4ba6d8bba89035f547ce87e9b4cfb614099d7
reactojango <adamgrizzard2@gmail.com>
2018-03-01 05:07:59 +0800

    feat(dwolla): reuse dwolla token in api requests

--- functions/dwolla/api.js
+++ functions/dwolla/api.js
@@ -1,14 +1,45 @@
 const dwolla = require('dwolla-v2');
+const moment = require('moment');
 const config = require('../config');
+const ref = require('../ref');
 const client = new dwolla.Client({
     key: config.dwolla.key,
     secret: config.dwolla.secret,
     environment: config.dwolla.environment
 });
 
-// @TODO reuse access token
+export function updateToken() {
+    return client.auth.client()
+    .then(token => {
+        const updates = {
+            dwolla_access: {
+                token: token.access_token,
+                timestamp: new Date().valueOf()
+            }
+        };
+        return ref.update(updates)
+        .then(() => token);
+    })
+}
+
 export function getAPIClient() {
-    return client.auth.client();
+    return ref.child('dwolla_access')
+        .once('value')
+        .then(snap => snap.val())
+        .then(dwollaAccess => {
+            if (!dwollaAccess) return updateToken();
+
+            const lastFetchTime = moment(dwollaAccess.timestamp, 'x');
+            const timeDiff = moment().diff(lastFetchTime, 'seconds');
+            if (timeDiff >= 3600) {
+                return updateToken();
+            }
+
+            return new client.Token({
+                access_token: dwollaAccess.token,
+                expires_in: timeDiff
+            });
+        });
 }
 
 export default getAPIClient;
