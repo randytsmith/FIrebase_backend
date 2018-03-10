d275229c4e30452975fcdd626e4f9a7fa0508b18
reactojango <adamgrizzard2@gmail.com>
2018-03-09 21:02:57 -0500

    changed test func

--- functions/dwolla/functions/test_plaid.js
+++ functions/dwolla/functions/test_plaid.js
@@ -1,4 +1,3 @@
-const ref = require('../../ref');
 const { getPlaidClient } = require('../api');
 
 /**
@@ -8,16 +7,8 @@ const { getPlaidClient } = require('../api');
  * @returns {Promise<string>} promise of customerID added
  */
 function testPlaid(userID, fundData) {
-    return getPlaidClient().then(fundInfo => {
-        const fundId = fundInfo[0].substr(fundInfo[0].lastIndexOf('/') + 1);
-        console.log(fundData);
-        return ref
-            .child('dwolla')
-            .child('customers^funding_sources')
-            .child(fundInfo[1])
-            .child(fundId)
-            .set({ status: 'pending' })
-            .then(() => fundId);
+    return getPlaidClient().then(client => {
+        return client.createPublicToken('testvalue').then(() => fundData);
     });
 }
 
