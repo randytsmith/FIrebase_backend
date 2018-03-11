7e25ec7602e3203645305027ab7454b5ff2e4805
reactojango <adamgrizzard2@gmail.com>
2018-03-12 00:06:24 +0800

    fix(dwolla): customer verified webhook styling

--- functions/dwolla/webhook_handlers/customer_verified.js
+++ functions/dwolla/webhook_handlers/customer_verified.js
@@ -11,15 +11,15 @@ function customerVerifiedWebhook(body) {
     const customerID = body.resourceId;
 
     return getAPIClient()
-    .then(client => client.get(`${config.dwolla.url}/funding-sources`))
-    .then(res => {
-        const holdingID = res.body._embedded['funding-sources'][0].id;
-        const updates = {};
+        .then(client => client.get(`${config.dwolla.url}/funding-sources`))
+        .then(res => {
+            const holdingID = res.body._embedded['funding-sources'][0].id;
+            const updates = {};
 
-        updates[`dwolla/customers/${customerID}/status`] = 'verified';
-        updates[`dwolla/customers^dwolla_holdings/${customerID}`] = holdingID;
-        return ref.update(updates);
-    });
+            updates[`dwolla/customers/${customerID}/status`] = 'verified';
+            updates[`dwolla/customers^dwolla_holdings/${customerID}`] = holdingID;
+            return ref.update(updates);
+        });
 }
 
 module.exports = customerVerifiedWebhook;
