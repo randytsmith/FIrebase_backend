98da9580417583a1cfb5e9d8214689673936552b
reactojango <adamgrizzard2@gmail.com>
2018-03-13 16:14:56 +0800

    feat(sendgrid): added mailer module

--- functions/mailer/index.js
+++ functions/mailer/index.js
@@ -1,17 +1,47 @@
-// @TODO use sendgrid instead of gmail
-// @TODO finish sendmail
-const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
 const config = require('../config');
 
-const mailTransport = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: config.gmail.email,
-        pass: config.gmail.password
-    }
-});
+sgMail.setApiKey(config.sendgrid.key);
 
-function sendMail(email, topic){
+/**
+ * @param {Array<string>} to
+ * @param {string} subject
+ * @param {string} text
+ * @param {string} html html can be duplicate of text
+ */
+function sendHTML(to, subject, text, html) {
+    const msg = {
+        to,
+        from: config.sendgrid.from,
+        subject,
+        text,
+        html: html || text
+    };
+    return sgMail.send(msg);
 }
 
-export default sendMail;
+/**
+ * @param {Array<string>} to
+ * @param {string} subject
+ * @param {string} templateId
+ * @param {Object} emailVars
+ * @param {string} text
+ * @param {string} html html can be duplicate of text
+ */
+function sendTemplate(to, subject, templateId, emailVars, text, html) {
+    const msg = {
+        to,
+        from: config.sendgrid.from,
+        subject,
+        text: text || '',
+        html: html || text || '',
+        templateId,
+        substitutions: emailVars
+    };
+    return sgMail.send(msg);
+}
+
+module.exports = {
+    sendHTML,
+    sendTemplate
+};
