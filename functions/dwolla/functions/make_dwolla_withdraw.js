4871ac54fd27fc093d0ef24c0cc3f450b9c193fe
reactojango <adamgrizzard2@gmail.com>
2018-03-14 16:14:10 -0400

    (dwolla) deploying dwolla withdraw

--- functions/dwolla/functions/make_dwolla_withdraw.js
+++ functions/dwolla/functions/make_dwolla_withdraw.js
@@ -1,8 +1,7 @@
-const moment = require('moment');
 const ref = require('../../ref');
 const { getAPIClient } = require('../api');
 const config = require('../../config');
-const { getCustomerID, getCustomerHoldingID } = require('../utils');
+const { getCustomerHoldingID } = require('../utils');
 
 /**
  * handles customer_activated event from dwolla
@@ -13,34 +12,25 @@ const { getCustomerID, getCustomerHoldingID } = require('../utils');
 function makeDwollaWithdraw(userID, transferData) {
     return getAPIClient()
         .then(client => {
-            return getCustomerID(userID).then(customerId => {
-                return getCustomerHoldingID(customerId).then(holdingId => {
-                    const requestBody = {
-                        _links: {
-                            source: {
-                                href: `${config.dwolla.url}/funding-sources/${holdingId}`
-                            },
-                            destination: {
-                                href: `${config.dwolla.url}/funding-sources/${transferData.fund}`
-                            }
+            return getCustomerHoldingID(transferData.customer_id).then(holdingId => {
+                const requestBody = {
+                    _links: {
+                        source: {
+                            href: `${config.dwolla.url}/funding-sources/${holdingId}`
                         },
-                        amount: {
-                            currency: 'USD',
-                            value: transferData.amount
-                        },
-                        metadata: {
-                            note: `One time transfer on ${moment().format('MM/DD/YYYY')}`
-                        },
-                        clearing: {
-                            destination: 'next-available',
-                            source: 'standard'
+                        destination: {
+                            href: `${config.dwolla.url}/funding-sources/${transferData.fund}`
                         }
-                    };
-                    return client.post('transfers', requestBody).then(res => {
-                        const transferUrl = res.headers.get('location');
-                        const transferId = transferUrl.substr(transferUrl.lastIndexOf('/') + 1);
-                        return [customerId, transferId];
-                    });
+                    },
+                    amount: {
+                        currency: 'USD',
+                        value: transferData.amount
+                    }
+                };
+                return client.post('transfers', requestBody).then(res => {
+                    const transferUrl = res.headers.get('location');
+                    const transferId = transferUrl.substr(transferUrl.lastIndexOf('/') + 1);
+                    return transferId;
                 });
             });
         })
@@ -48,10 +38,10 @@ function makeDwollaWithdraw(userID, transferData) {
             return ref
                 .child('dwolla')
                 .child('customers^transfers')
-                .child(transfer[0])
-                .child(transfer[1])
+                .child(transferData.customer_id)
+                .child(transfer)
                 .set({ amount: transferData.amount, status: 'pending', type: 'withdraw' })
-                .then(() => transfer[1]);
+                .then(() => transfer);
         });
 }
 
