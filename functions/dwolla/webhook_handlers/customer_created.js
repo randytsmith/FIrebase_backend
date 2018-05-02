1571939cd8475f0b0865ef15a5b11b8657b17b39
reactojango <adamgrizzard2@gmail.com>
2018-05-01 22:19:02 -0400

    fixed create customer

--- functions/dwolla/webhook_handlers/customer_created.js
+++ functions/dwolla/webhook_handlers/customer_created.js
@@ -2,6 +2,7 @@ const ref = require('../../ref');
 const mailer = require('../../mailer');
 // const fcm = require('../../fcm');
 const utils = require('../utils');
+const { getCustomer } = require('../utils');
 
 /**
  * handles customer_created event from dwolla
@@ -12,24 +13,32 @@ const utils = require('../utils');
 // the verification webhooks sometimes come before created
 function customerCreatedWebhook(body) {
     const customerID = body.resourceId;
-    const updates = {};
-    updates[`dwolla/customers/${customerID}/status`] = 'created';
-    updates[`dwolla/customers/${customerID}/balance`] = 0;
-    utils.getUserID(customerID).then(userID => {
-        console.log('sending email and push notification');
-        // fcm.sendNotificationToUser(userID, 'Tripents Savings Created!', 'Your dwolla account has been created!').catch(err => console.error(err));
-        const message =
-            'Congratulations! You’ve successfully opened a travel fund. \
-            You’re one step closer to that dream trip you’ve always kept on \
-            the backburners, so give yourself a pat on the back. This email \
-            also confirms that you accept our partner Dwolla’s \
-            Terms of Service and Privacy Policy. Thanks! -The Tripcents team';
-        const bodyDict = {};
-        mailer
-            .sendTemplateToUser(userID, 'Dwolla account verified!', '196a1c48-5617-4b25-a7bb-8af3863b5fcc', bodyDict, ' ', message)
-            .catch(err => console.error(err));
+    return getCustomer(customerID).then(customer_info => {
+        let status = '';
+        if (customer_info.status === 'verified') {
+            status = 'verified';
+        } else {
+            status = 'created';
+        }
+        const updates = {};
+        updates[`dwolla/customers/${customerID}/status`] = status;
+        updates[`dwolla/customers/${customerID}/balance`] = 0;
+        utils.getUserID(customerID).then(userID => {
+            console.log('sending email and push notification');
+            // fcm.sendNotificationToUser(userID, 'Tripents Savings Created!', 'Your dwolla account has been created!').catch(err => console.error(err));
+            const message =
+                'Congratulations! You’ve successfully opened a travel fund. \
+                You’re one step closer to that dream trip you’ve always kept on \
+                the backburners, so give yourself a pat on the back. This email \
+                also confirms that you accept our partner Dwolla’s \
+                Terms of Service and Privacy Policy. Thanks! -The Tripcents team';
+            const bodyDict = {};
+            mailer
+                .sendTemplateToUser(userID, 'Dwolla account verified!', '196a1c48-5617-4b25-a7bb-8af3863b5fcc', bodyDict, ' ', message)
+                .catch(err => console.error(err));
+        });
+        return ref.update(updates);
     });
-    return ref.update(updates);
 }
 
 module.exports = customerCreatedWebhook;
